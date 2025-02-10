6293921ef0484c43007200399318fe1b2f8835ab
prasanth150202 <168893891+prasanth150202@users.noreply.github.com>
2025-02-10 14:47:30 +0530

    Last update

--- scroll.js
+++ scroll.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 gsap.registerPlugin(ScrollTrigger);
 
 let horizontalSection = document.querySelector('.horizontal');
@@ -33,39 +32,3 @@ const navbarMenu = document.getElementById('navbarNav');
 toggleButton.addEventListener('click', () => {
     navbarMenu.classList.toggle('show');
 });
-=======
-gsap.registerPlugin(ScrollTrigger);
-
-let horizontalSection = document.querySelector('.horizontal');
-let totalWidth = horizontalSection.scrollWidth;
-
-function getXValue() {
-    if (window.innerWidth <= 768) { 
-        return -(totalWidth - window.innerWidth / 1.1); // Adjust for mobile
-    } else {
-        return -(totalWidth - window.innerWidth / 1.5); // Default for larger screens
-    }
-}
-
-gsap.to('.horizontal', {
-    x: getXValue,
-    ease: "none",
-    scrollTrigger: {
-        trigger: '.horizontal-scoll-wrapper',
-        start: 'top 25%',
-        end: () => "+=" + (totalWidth - window.innerWidth / (window.innerWidth <= 768 ? 1 : 1.2)), 
-        pin: '.horizontal-scoll-wrapper',
-        scrub: 1, 
-        markers: false,
-        invalidateOnRefresh: true,
-    }
-});
-
-
-const toggleButton = document.getElementById('navbar-toggler');
-const navbarMenu = document.getElementById('navbarNav');
-
-toggleButton.addEventListener('click', () => {
-    navbarMenu.classList.toggle('show');
-});
->>>>>>> a094891 (Initial commit - pushing entire folder)
